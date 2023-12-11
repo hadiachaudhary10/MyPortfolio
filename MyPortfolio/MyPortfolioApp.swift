6da2e646566e012db4c3bc10bed892ea00501c52
Hadia Chaudhary <hadia.chaudhary@devsinc.com>
2023-12-11 12:56:54 +0500

    Initial commit

---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by Dev on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
